edbb4fa7f3447aaadf923225fa54ec20690b7a8e
Sonu Kushwaha <imsonukushwaha@gmail.com>
2024-12-18 22:56:17 +0530

    signin/signup integrated

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,11 +1,24 @@
-import { Text, View } from "react-native";
+import { SignedIn, SignedOut, useUser } from "@clerk/clerk-expo";
+import { Link } from "expo-router";
+import React from "react";
+import { StatusBar, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Home = () => {
+  const { user } = useUser();
   return (
-    <SafeAreaView>
-      <Text className="self-center">Home</Text>
-    </SafeAreaView>
+    <>
+      <StatusBar
+        translucent
+        backgroundColor="transparent"
+        barStyle="dark-content"
+      />
+      <SafeAreaView>
+        <Text className="self-center">
+          {user?.emailAddresses[0]?.emailAddress}
+        </Text>
+      </SafeAreaView>
+    </>
   );
 };
 
--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,13 @@
+import { useAuth } from "@clerk/clerk-expo";
 import { Redirect } from "expo-router";
-import { Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
 
 const Index = () => {
+  const { isSignedIn } = useAuth();
+
+  if (isSignedIn) {
+    return <Redirect href="/(root)/(tabs)/home" />;
+  }
+
   return <Redirect href="/(auth)/welcome" />;
 };
 
